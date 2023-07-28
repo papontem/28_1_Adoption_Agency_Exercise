cd78380caad1569d03e6c37194a6f79c3da2e8b1
papontemjetix <papontem@gmail.com>
2023-07-27 23:15:24 -0400

    part 4 and 5 done, validation contraints and error showing on pet add form.

--- forms.py
+++ forms.py
@@ -1,15 +1,26 @@
-"""Forms for our demo Flask app."""
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
-from wtforms.validators import InputRequired, Optional
-
+from wtforms.validators import InputRequired, Optional, URL, NumberRange, AnyOf
 
 class AddPetsForm(FlaskForm):
     """Form for adding Pets"""
 
-    name      = StringField("Pet name"         , validators=[InputRequired(message="Name cannot be blank")])
-    species   = StringField("Pet species"      , validators=[InputRequired(message="Species cannot be blank")])
-    photo_url = StringField("Link photo of pet", validators=[Optional()])
-    age       = FloatField( "Pet age in years" , validators=[Optional()])
-    notes     = StringField("any notes?"       , validators=[Optional()])
+    species_choices = ['cat', 'dog', 'porcupine']
+
+    name = StringField("Pet name",
+                        validators=[InputRequired(message="Name cannot be blank")])
+    
+    species = StringField("Pet species",
+                           validators=[
+                               InputRequired(message="Species cannot be blank"),
+                               AnyOf(species_choices, message="We are only accepting pet species of 'cat', 'dog' and 'porcupine'")
+                               ])
+    
+    photo_url = StringField("Link photo of pet", 
+                            validators=[Optional(), URL(message="Invalid URL")])
+    
+    age = FloatField("Pet age in years",
+                      validators=[Optional(), NumberRange(min=0, max=30, message="Age must be between 0 and 30 years of age.")])
+    
+    notes = StringField("any notes?", 
+                        validators=[Optional()])
