529ddbc9bfc9cb173b9ce3536a595611676a49f4
papontemjetix <papontem@gmail.com>
2023-07-27 22:53:55 -0400

    part 3 done, pet add form complete

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from flask import Flask, render_template, flash, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 
-from forms import AddSnackForm
-from forms import UserForm
+from forms import AddPetsForm
 
 app = Flask(__name__)
+# FLASK SETTINGS
 app.config["SECRET_KEY"] = "oh-so-secret"
+# FLASK-SQLALCHEMY SETTINGS
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///pet_adoption_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# FLASK-DEBUGTOOLBAR SETTINGS
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app) 
 
-debug = DebugToolbarExtension(app)
-
+# flask-sqlalchemy init app db
 connect_db(app)
 
 
@@ -24,21 +27,29 @@ def homepage():
     return render_template("home.html",pets=pets)
 
 
-# @app.route("/add", methods=["GET", "POST"])
-# def add_snack():
-#     """Snack add form; handle adding."""
+@app.route("/add", methods=["GET", "POST"])
+def add_snack():
+    """ Pet add form; handle adding."""
 
-#     form = AddSnackForm()
+    form = AddPetsForm()
 
-#     if form.validate_on_submit():
-#         name = form.name.data
-#         price = form.price.data
-#         flash(f"Added {name} at {price}")
-#         return redirect("/add")
-
-#     else:
-#         return render_template(
-#             "snack_add_form.html", form=form)
+    if form.validate_on_submit():
+        # form was valid, get its data
+        name      = form.name.data
+        species   = form.species.data 
+        photo_url = form.photo_url.data
+        age       = form.age.data
+        notes     = form.notes.data
+        # create new pet
+        new_pet = Pet(name=name, species=species, age=age, notes=notes, photo_url=photo_url)
+        db.session.add(new_pet)
+        db.session.commit()
+        # flash user success
+        flash(f"Added a New Pet!", "success")
+        # redirect
+        return redirect("/")
+    else:
+        return render_template("add_pet_form.html", form=form)
 
 
 # @app.route("/users/<int:uid>/edit", methods=["GET", "POST"])
--- forms.py
+++ forms.py
@@ -2,20 +2,14 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
-from wtforms.validators import InputRequired, Optional, Email
+from wtforms.validators import InputRequired, Optional
 
 
-class AddSnackForm(FlaskForm):
-    """Form for adding snacks."""
+class AddPetsForm(FlaskForm):
+    """Form for adding Pets"""
 
-    name = StringField("Snack Name")
-    price = FloatField("Price in USD")
-
-
-class UserForm(FlaskForm):
-    """Form for adding/editing friend."""
-
-    name = StringField("Name",
-                       validators=[InputRequired()])
-    email = StringField("Email Address",
-                        validators=[Optional(), Email()])
+    name      = StringField("Pet name"         , validators=[InputRequired(message="Name cannot be blank")])
+    species   = StringField("Pet species"      , validators=[InputRequired(message="Species cannot be blank")])
+    photo_url = StringField("Link photo of pet", validators=[Optional()])
+    age       = FloatField( "Pet age in years" , validators=[Optional()])
+    notes     = StringField("any notes?"       , validators=[Optional()])
